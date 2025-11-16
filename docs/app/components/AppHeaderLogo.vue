eff87fd075cc446de2fa55129d378f25600c46a3
Hugo Richard <hugo.richard@vercel.com>
2025-11-16 16:10:48 +0000

    improve docs appearance

---
+++ docs/app/components/AppHeaderLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center gap-2">
+    <UIcon name="custom:mcp" />
+    <span>Nuxt MCP</span>
+  </div>
+</template>
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,39 @@
+export default defineNuxtConfig({
+  extends: ['docus'],
+
+  modules: ['@hrcd/mcp'],
+
+  css: ['~/assets/css/main.css'],
+
+  site: {
+    name: 'Nuxt MCP Module',
+  },
+
+  icon: {
+    customCollections: [
+      {
+        prefix: 'custom',
+        dir: './app/assets/icons',
+      },
+    ],
+    clientBundle: {
+      scan: true,
+      includeCustomCollections: true,
+    },
+    provider: 'iconify',
+  },
+
+  llms: {
+    domain: 'https://nuxt-mcp-module.vercel.app',
+    title: 'Nuxt MCP module',
+    description: 'Create MCP servers directly in your Nuxt application. Define tools, resources, and prompts with a simple and intuitive API.',
+    full: {
+      title: 'Nuxt MCP module',
+      description: 'Create MCP servers directly in your Nuxt application. Define tools, resources, and prompts with a simple and intuitive API.',
+    },
+  },
+
+  mcp: {
+    name: 'Nuxt MCP Module',
+  },
+})
