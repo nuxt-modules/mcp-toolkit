a831d03833ff6e9d60dda781101db7c6e3e5855b
Hugo Richard <hugo.richard@vercel.com>
2025-11-24 11:13:10 +0100

    feat(docs): landing page

---
+++ apps/docs/app/components/content/landing/LandingDevTools.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// @ts-expect-error yaml is not typed
+import devtools from './devtools.yml'
+</script>
+
+<template>
+  <UPageSection
+    :title="devtools.title"
+    :description="devtools.description"
+    :ui="{ container: 'lg:py-20' }"
+  >
+    <UColorModeImage
+      :light="devtools.image.light"
+      :dark="devtools.image.dark"
+      :alt="devtools.image.alt"
+      class="w-full h-auto grayscale"
+    />
+  </UPageSection>
+</template>
